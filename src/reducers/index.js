8fda89e44e6ebadb4bfe464426eaa99209798e31
Harrison Bacordo <harrisonbacordo@gmail.com>
2018-09-06 12:35:56 +1200

    Added more reducers to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,15 @@ import authReducer from './authReducer'
 import choresReducer from './choresReducer';
 import remindersReducers from './remindersReducer';
 import groceriesReducer from './groceriesReducer';
+import flatmateReducer from "./flatmateReducer";
+import flatReducer from "./flatReducer";
 
 export default combineReducers({
     auth: authReducer,
     chores: choresReducer,
     reminders: remindersReducers,
-    groceries: groceriesReducer
+    groceries: groceriesReducer,
+    flat: flatReducer,
+    flatmates: flatmateReducer,
+
 });
